259245775e6a1afc7ee54f0add22a90d56777125
cepthomas <cepthomas@gmail.com>
2024-07-05 14:41:50 -0500

    Names and docs.

--- UserSettings.cs
+++ UserSettings.cs
@@ -17,7 +17,7 @@ namespace Wavicler
     {
         #region Persisted Editable Properties
         [DisplayName("Control Color")]
-        [Description("Pick what you like.")]
+        [Description("The color used for active control surfaces.")]
         [Browsable(true)]
         [JsonConverter(typeof(JsonColorConverter))]
         public Color ControlColor { get; set; } = Color.MediumOrchid;
